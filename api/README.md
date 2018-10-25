145805006a86e89a6599a68f845a39989e2128d3
joshsisley <josh@joshuasisley.com>
2018-10-25 16:54:28 -0400

    initial commit of project

---
+++ api/README.md
@@ -0,0 +1,6 @@
+# Weather API
+
+API backend for [Weather Vue](https://github.com/krestaino/weather-vue). Please refer to this repo for project setup.
+
+## License 
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ config/prod.env.js
@@ -0,0 +1,10 @@
+var keys = require('../api/keys')
+
+module.exports = {
+  NODE_ENV: '"production"',
+  API_KEY: keys.prod,
+  API_URL: {
+    geocode: '"https://api.weather.kmr.io/geocode/v1/json?"',
+    weather: '"https://api.weather.kmr.io/weather/v1/json?"'
+  }
+}
